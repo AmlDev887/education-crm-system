68afe280babab521d2e5b517c555acce6dce1a81
AmlDev887 <xolmurodovamalbek494@gmail.com>
2026-08-11 12:49:47 +0500

    JWT tokens,docker,docker compise

--- beckend_crm/database.py
+++ beckend_crm/database.py
@@ -1,14 +1,22 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-# database.py
-DATABASE_URL = "postgresql+psycopg2://postgres:postgrespassword@db:5432/crm_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg2://postgres:postgrespassword@localhost:5433/crm_db"
+)
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
 class Base(DeclarativeBase):
     pass
+
 
 def get_db():
     db = SessionLocal()
